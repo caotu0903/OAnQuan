a14976329fa21d1105d85cac78a3fcdfdcbe7fcc
ASUS <caotu0903@gmail.com>
2021-12-11 16:02:50 +0700

    main branch

--- core/src/com/mygdx/gameco/GameScreen.java
+++ core/src/com/mygdx/gameco/GameScreen.java
@@ -5,6 +5,8 @@ import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
@@ -17,23 +19,38 @@ public class GameScreen implements Screen {
     //graphics
     private SpriteBatch batch;
     private Texture background;
+    private TextureAtlas textureAtlas;
+
+    private TextureRegion oCoTextureRegion,backGroundRegion;
 
     //timing
     private int backgroundOffset;
 
     //world parameters
-    private final int WORLD_WIDTH = 72;
-    private final int WORLD_HEIGHT = 128;
+    private final int WORLD_WIDTH = 128;
+    private final int WORLD_HEIGHT = 72;
+
+    //game objects
+    private OCo oCo;
 
     GameScreen() {
 
         camera = new OrthographicCamera();
         viewport = new StretchViewport(WORLD_WIDTH,WORLD_HEIGHT,camera);
 
-        background = new Texture("o-an-quan.jpg");
+        //set up the texture atlas
+        textureAtlas = new TextureAtlas("image.atlas");
+
+        //initialize texture regions
+        oCoTextureRegion = textureAtlas.findRegion("5-rock");
+
+        backGroundRegion = textureAtlas.findRegion("o-an-quan");
         backgroundOffset = 0;
 
         batch = new SpriteBatch();
+
+        //set up game objects
+        oCo = new OCo (5, false, false, WORLD_WIDTH*0.386f, WORLD_HEIGHT*0.4f, 15, 15, oCoTextureRegion);
 
     }
 
@@ -41,15 +58,11 @@ public class GameScreen implements Screen {
     public void render(float delta) {
         batch.begin();
 
-        //scrolling background
-        /*backgroundOffset ++;
-        if (backgroundOffset % WORLD_HEIGHT == 0) {
-            backgroundOffset = 0;
-        }*/
+        //Background
+        batch.draw(backGroundRegion, 0,0,WORLD_WIDTH,WORLD_HEIGHT);
 
-
-        batch.draw(background, 0,0,WORLD_WIDTH,WORLD_HEIGHT);
-
+        //O Co
+        oCo.draw(batch);
 
         batch.end();
     }
--- core/src/com/mygdx/gameco/OCo.java
+++ core/src/com/mygdx/gameco/OCo.java
@@ -1,13 +1,32 @@
 package com.mygdx.gameco;
 
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Rectangle;
+
 public class OCo {
 
     //OCo characteristics
+    int numberCo;
     boolean isQuan, isAliveQuan;
 
     //position & dimension
-    float xPosition, yPosition; //lower-left corner;
-
+    Rectangle boundingBox;
 
     //graphics
+    TextureRegion ocoTexture;
+
+    public OCo(int numberCo, boolean isQuan, boolean isAliveQuan, float xCenter, float yCenter, float width, float height, TextureRegion ocoTexture) {
+        this.numberCo = numberCo;
+        this.isQuan = isQuan;
+        this.isAliveQuan = isAliveQuan;
+
+        this.boundingBox = new Rectangle(xCenter - width/2, yCenter - height/2, width, height);
+
+        this.ocoTexture = ocoTexture;
+    }
+
+    public void draw(Batch batch) {
+        batch.draw(ocoTexture, boundingBox.x, boundingBox.y, boundingBox.width, boundingBox.height);
+    }
 }
