b29523998fd33e429f0ee7aee96dc010309b79f8
caotu0903 <62198989+caotu0903@users.noreply.github.com>
2021-12-11 12:35:07 +0700

    Add files via upload

---
+++ DoAn/android/src/com/mygdx/gameco/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.gameco;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.gameco.OAnQuanGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new OAnQuanGame(), config);
+	}
+}
---
+++ DoAn/core/src/com/mygdx/gameco/OCo.java
@@ -0,0 +1,13 @@
+package com.mygdx.gameco;
+
+public class OCo {
+
+    //OCo characteristics
+    boolean isQuan, isAliveQuan;
+
+    //position & dimension
+    float xPosition, yPosition; //lower-left corner;
+
+
+    //graphics
+}
